0f16689e19240235de7a8530e594130c13da2c0b
mihaimiron1 <mihaimiron004@gmail.com>
2026-02-05 14:21:10 +0200

    Add comment to addItem method in Catalog interface

--- TMPP_Project/demo/src/main/java/com/mihai/library/repo/Catalog.java
+++ TMPP_Project/demo/src/main/java/com/mihai/library/repo/Catalog.java
@@ -6,7 +6,7 @@ import java.util.Optional;
 import com.mihai.library.domain.LibraryItem;
 
 public interface Catalog {
-    void addItem(LibraryItem item);
+    void addItem(LibraryItem item); // add item in catalog
     Optional<LibraryItem> findById(String id);
     List<LibraryItem> getAllItems();
 }
